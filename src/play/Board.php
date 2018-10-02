6ee5ae3e1174367815e21d3b2b72e19ecae2c8f1
uribea <aauribe@miners.utep.edu>
2018-10-02 15:45:25 -0600

    try fix board

--- src/play/Board.php
+++ src/play/Board.php
@@ -8,12 +8,17 @@
 
 class Board
 {
-    var $player=0;//keep track of player
-    var $board=array();
-    var $row=7;
-    var $col=6;
+    public  $player=0;//keep track of player
+    public  $board;
+    public  $row=7;
+    public $col=6;
     //var $total=42;
 
- 
+    function  __construct()
+    {
+        $this->board=array_fill(0,42,0);
+    }
+
+
 
 }
