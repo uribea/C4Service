454787e7539a21b08d774a6d7e569941a284eab7
pldelao <35977180+pldelao@users.noreply.github.com>
2018-10-02 15:42:04 -0600

    function

---
+++ Board.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: perladelao
+ * Date: 9/30/18
+ * Time: 10:52 AM
+ */
+
+class Board
+{
+    public  $player=0;//keep track of player
+    public  $board;
+    public  $row=7;
+    public $col=6;
+    //var $total=42;
+
+     function  __construct()
+     {
+         $this->board=array_fill(0,42,0);
+     }
+
+
+
+}
