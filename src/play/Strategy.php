f36eb54300f1ba60edb418d954e5fb724fc4a3af
Germain <germain.v40@gmail.com>
2018-10-02 21:02:30 -0600

    updated strategy

--- src/play/Strategy.php
+++ src/play/Strategy.php
@@ -6,8 +6,25 @@
  * Time: 11:25 AM
  */
 
-abstract class Strategy extends
+abstract class Strategy
 {
+    var $board;
+
+    function __construct(Board $board = null) {
+        $this->board = $board;
+    }
+
+    abstract function pickSlot();
+
+    function toJason() {
+        return array(‘name’ => get_class($this));
+    }
+
+    static function fromJson($obj) {
+        $strategy = new static();
+        return $strategy;
+    }
+
     function move()
     {
 
